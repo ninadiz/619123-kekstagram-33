6deb736ae53ab2738f662f881cbf3f565950c4e4
ninadiz <raven-night@mail.ru>
2024-10-21 11:55:56 +0300

    add previous work

---
+++ js/const.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+
+const IMAGE_LIST_LENGTH = 25;
+
+const LIKES_COUNT = {
+  min: 15,
+  max: 200,
+};
+
+const COMMENTS_COUNT = {
+  min: 0,
+  max: 30,
+};
+
+export {IMAGE_LIST_LENGTH, LIKES_COUNT, COMMENTS_COUNT};
---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Загружает все модули в index.html
+import './const.js';
+import './lorem-upsum-data.js';
+import './random-number-from-range.js';
+import './random-array-element.js';
+import './code.js';
